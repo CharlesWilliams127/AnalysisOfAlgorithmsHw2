3409abe2af937f8c47be1cc4e05bc401a3774120
Charles Williams <crw4147@rit.edu>
2018-02-28 19:27:29 -0500

    Add project files.

---
+++ AnalysisOfAlgorithmsHw2/Source.cpp
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <vector>
+
+using namespace std;
+
+// define size of array
+const int SIZE = 10;
+
+// final longest increasing sequence array
+vector<int> lisArr;
+
+void lis(int V[], int n)
+{
+	//int * r = (int*)malloc(sizeof(int) * n);
+	////int * s = (int*)malloc(sizeof(int) * n);
+	//
+	//memset(r, 1, n * sizeof(int));
+	//
+	//lisArr = vector<int>();
+	//
+	//for (int i = 1; i < n; i++)
+	//{
+	//	for (int j = 0; j < i; j++)
+	//	{
+	//		if (V[i] > V[j] && r[i] < r[j] + 1)
+	//		{
+	//			r[i] = r[j] + 1;
+	//			lisArr.push_back(V[j]);
+	//		}
+	//	}
+	//}
+
+	//vector<vector<int>> L(n);
+	//
+	//L[0].push_back(V[0]);
+
+
+}
+
+int main()
+{
+	// allocate space for array
+	int * initialArray = new int[SIZE];
+
+	// populate array
+	for (int i = 0; i < SIZE; i++)
+	{
+		initialArray[i] = rand() % 10 + 1;
+	}
+
+	printf("Initial Array:\n");
+	for (int i = 0; i < SIZE; i++)
+	{
+		printf("%d\n", initialArray[i]);
+	}
+
+	// call naive LIS
+	lis(initialArray, SIZE);
+
+	// print both
+	
+
+	printf("Final LIS Array:\n");
+	for (int i = 0; i < lisArr.size(); i++)
+	{
+		printf("%d\n", lisArr[i]);
+	}
+
+	// de-allocate initialArray
+	delete[] initialArray;
+
+	system("pause");
+
+	return 0;
+}
