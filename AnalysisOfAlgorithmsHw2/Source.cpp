a491a13d9045d999644645df6ada927506fe73f0
crw4147 <crw4147@rit.edu>
2018-03-04 12:36:45 -0500

    Implemented LIS problem

--- AnalysisOfAlgorithmsHw2/Source.cpp
+++ AnalysisOfAlgorithmsHw2/Source.cpp
@@ -5,69 +5,89 @@
 
 using namespace std;
 
-// define size of array
-const int SIZE = 10;
+// file name of LIS question for problem two
+const char* lisFileName = "something"; // TODO: add file name/reading
+const char* morseCodeFileName = "something"; // TODO: add file name/reading
 
-// final longest increasing sequence array
-vector<int> lisArr;
-
-void lis(int V[], int n)
+/// My implementation of LIS includes a vector of vectors to eliminate
+/// recursion and dynamically program this function. Each vector within
+/// the outer vector stores a potential LIS, which we compute by comparing
+/// our current element to all previous elements in a bottom-up approach
+/// params:
+/// V : the initial array to iterate through
+/// n : the size of the initial array
+/// return value: the LIS of V
+vector<int> lis(int V[], int n)
 {
-	int * r = (int*)malloc(sizeof(int) * n);
-	//int * s = (int*)malloc(sizeof(int) * n);
+	int  i, j;
+	vector<int> maxLIS;
 
-	for (int i = 0; i < n; i++)
+	// contains each potential lis
+	vector<vector<int>> candidateLIS = vector<vector<int>>(n);
+
+	// our first sub-sequence is just our first element, since we 
+	// have no other elements to compare it to
+	candidateLIS[0].push_back(V[0]);
+
+	// this means by default, our max value is the first element in 
+	// our vector
+	maxLIS = candidateLIS[0];
+
+	// as opposed to recursion, since this is dynamically programmed
+	// compute the values in or vector of subsequences in a bottom-up
+	// approach, this eliminates recursion and allows us to store the 
+	// results of each subsequence
+	for (i = 1; i < n; i++)
 	{
-		r[i] = 1;
-	}
-	
-	lisArr = vector<int>();
-	
-	for (int i = 1; i < n; i++)
-	{
-		for (int j = 0; j < i; j++)
+		for (j = 0; j < i; j++)
 		{
-			if (V[i] > V[j] && r[i] < r[j] + 1)
+			if ((V[i] > V[j]) &&
+				(candidateLIS[i].size() < candidateLIS[j].size() + 1))
 			{
-				r[i] = r[j] + 1;
+				// we've found a replacement candidate if we reach here,
+				// this works similarly to pushing V[j] however, with just
+				// directly assigning the vector we can continuously
+				// overwrite the previous value, preventing values no longer
+				// in the sequence from persisting
+				candidateLIS[i] = candidateLIS[j];
 			}
+		}
+		// to prevent loss of last element
+		candidateLIS[i].push_back(V[i]);
+	}
+
+	// iterate through, comparing sizes of all candidates to current
+	// max, if any of them is larger then it becomes the new max
+	for (i = 0; i < n; i++)
+	{
+		if (maxLIS.size() < candidateLIS[i].size())
+		{
+			maxLIS = candidateLIS[i];
 		}
 	}
 
-	delete[] r;
+	return maxLIS;
 }
 
+int morseCode(char V[], int n)
+{
+
+}
+
+/// main works as the driver for both of our algorithms in this case.
 int main()
 {
 	// allocate space for array
-	int * initialArray = new int[SIZE];
+	int initialVay[] = { 9, 15, 3, 6, 4, 2, 5, 10, 3};
 
-	// populate array
-	for (int i = 0; i < SIZE; i++)
-	{
-		initialArray[i] = rand() % 10 + 1;
-	}
-
-	printf("Initial Array:\n");
-	for (int i = 0; i < SIZE; i++)
-	{
-		printf("%d\n", initialArray[i]);
-	}
-
-	// call naive LIS
-	lis(initialArray, SIZE);
-
-	// print both
+	// call LIS function
+	vector<int> maxLIS = lis(initialVay, 9);
 	
-
-	printf("Final LIS Array:\n");
-	for (int i = 0; i < lisArr.size(); i++)
+	printf("Final LIS:\n");
+	for (int i = 0; i < maxLIS.size(); i++)
 	{
-		printf("%d\n", lisArr[i]);
+		printf("%d\n", maxLIS[i]);
 	}
-
-	// de-allocate initialArray
-	delete[] initialArray;
 
 	system("pause");
 
